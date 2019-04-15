4cb35523125216d5c007b623406cc52754aed987
dianesabra <dwsabra@gmail.com>
2019-04-15 18:57:19 -0500

    this

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("./models");
-var PORT = 3000;
+var PORT = 3000 || process.env.PORT;
 var app = express();
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
